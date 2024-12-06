8eb1e584125ddcda2ac3b468ed333c9d5dbb55d7
Joshua Mitra <joshua@alphalayer.ai>
2024-12-06 15:26:04 -0700

    Reorganize and parallelize day6

--- src/day06/day06.py
+++ src/day06/day06.py
@@ -1,7 +1,11 @@
 import itertools
-from copy import copy
+from copy import copy, deepcopy
+from dataclasses import dataclass, field
 from math import sumprod
 from pathlib import Path
+
+from joblib import Parallel, delayed
+from tqdm import tqdm
 
 Coord = tuple[int, int]
 
@@ -13,7 +17,10 @@ class Direction:
 
     def get_rotated(self) -> "Direction":
         return Direction(
-            (int(sumprod(self._rotation_matrix[0], self.coord)), int(sumprod(self._rotation_matrix[1], self.coord)))
+            (
+                int(sumprod(self._rotation_matrix[0], self.coord)),
+                int(sumprod(self._rotation_matrix[1], self.coord)),
+            )
         )
 
     @property
@@ -27,6 +34,20 @@ class Direction:
     @property
     def sign(self) -> int:
         return self.coord[self.distance_indexer]
+
+
+@dataclass
+class GuardState:
+    """
+    ewwww state...
+    """
+
+    position: Coord
+    direction: Direction
+    visited: set[Coord] = field(default_factory=set)
+    objects_hit_and_direction: list[tuple[tuple[int, int], tuple[int, int]]] = field(default_factory=list)
+    has_exited: bool = False
+    is_looping: bool = False
 
 
 def read_input(path: Path) -> tuple[list[Coord], Coord, int]:
@@ -53,67 +74,73 @@ def get_visit_set(start: Coord, finish: Coord, direction: Direction) -> set[Coor
         return {(i, start[1]) for i in range(start[0], finish[0] + direction.sign, direction.sign)}
 
 
-def solve(pos: Coord, obstacles: list[Coord], direction: Direction, side_length: int, part2: bool) -> set[Coord] | bool:
-    steps = 0
-    spots_visited: set[Coord] = set()
-    obstacles_hit_and_directions: list[tuple[Coord, Coord]] = []
-    while True:
-        obstacles = sorted(obstacles, key=lambda o: abs(o[0] - pos[0]) + abs(o[1] - pos[1]))
-        obstacles_hit = list(
-            itertools.dropwhile(
-                lambda o: (pos[direction.collision_indexer] != o[direction.collision_indexer])
-                or ((o[direction.distance_indexer] - pos[direction.distance_indexer]) * direction.sign < 0),
-                obstacles,
-            )
+def update_guard_state(guard: GuardState, obstacles: list[Coord], side_length: int) -> GuardState:
+    direction = guard.direction
+    pos = guard.position
+    obstacles = sorted(obstacles, key=lambda o: abs(o[0] - pos[0]) + abs(o[1] - pos[1]))
+    obstacles_hit = list(
+        itertools.dropwhile(
+            lambda o: (pos[direction.collision_indexer] != o[direction.collision_indexer])
+            or ((o[direction.distance_indexer] - pos[direction.distance_indexer]) * direction.sign < 0),
+            obstacles,
         )
-        if not obstacles_hit:
-            if part2:
-                return False
-            new_pos_list = [0, 0]
-            new_pos_list[direction.collision_indexer] = pos[direction.collision_indexer]
-            if direction.sign > 0:
-                new_pos_list[direction.distance_indexer] = side_length - 1
-            else:
-                new_pos_list[direction.distance_indexer] = 0
-            new_pos = (new_pos_list[0], new_pos_list[1])
-            spots_visited.update(get_visit_set(pos, new_pos, direction))
-            break
-
-        obstacle_hit = obstacles_hit[0]
-        if part2:
-            if (obstacle_hit, direction.coord) in obstacles_hit_and_directions:
-                return True
-        obstacles_hit_and_directions.append((obstacle_hit, direction.coord))
-
-        new_pos_list = copy(list(obstacle_hit))
-        new_pos_list[direction.distance_indexer] -= direction.sign
+    )
+    if not obstacles_hit:
+        new_pos_list = [0, 0]
+        new_pos_list[direction.collision_indexer] = pos[direction.collision_indexer]
+        if direction.sign > 0:
+            new_pos_list[direction.distance_indexer] = side_length - 1
+        else:
+            new_pos_list[direction.distance_indexer] = 0
         new_pos = (new_pos_list[0], new_pos_list[1])
-        spots_visited.update(get_visit_set(pos, new_pos, direction))
-        direction = direction.get_rotated()
-        pos = new_pos
-        steps += 1
-    return spots_visited
+        guard.visited.update(get_visit_set(pos, new_pos, direction))
+        return GuardState(new_pos, direction.get_rotated(), guard.visited, has_exited=True)
+
+    obstacle_hit = obstacles_hit[0]
+    if (obstacle_hit, direction.coord) in guard.objects_hit_and_direction:
+        return GuardState(pos, direction, guard.visited, is_looping=True)
+    guard.objects_hit_and_direction.append((obstacle_hit, direction.coord))
+    new_pos_list = copy(list(obstacle_hit))
+    new_pos_list[direction.distance_indexer] -= direction.sign
+    new_pos = (new_pos_list[0], new_pos_list[1])
+    guard.visited.update(get_visit_set(pos, new_pos, direction))
+    return GuardState(
+        new_pos,
+        direction.get_rotated(),
+        guard.visited,
+        objects_hit_and_direction=guard.objects_hit_and_direction,
+    )
+
+
+def solve1(guard: GuardState, obstacles: list[Coord], side_length: int) -> set[Coord]:
+    while not guard.has_exited:
+        guard = update_guard_state(guard, obstacles, side_length)
+    return guard.visited
+
+
+def check_if_cycle(guard: GuardState, obstacles: list[Coord], side_length: int) -> bool:
+    while not (guard.has_exited or guard.is_looping):
+        guard = update_guard_state(guard, obstacles, side_length)
+    return guard.is_looping
+
+
+def solve2(guard: GuardState, obstacles: list[Coord], side_length: int, visited: set[Coord]) -> int:
+    visited.remove(guard.position)
+    cycle_count = sum(
+        # Nothing says I have a good algorithm like n_jobs=-1...
+        Parallel(n_jobs=-1)(
+            delayed(check_if_cycle)(deepcopy(guard), obstacles + [coord], side_length) for coord in tqdm(visited)
+        )
+    )
+    return cycle_count
 
 
 def main() -> None:
     maze, start, side_length = read_input(Path("data/input06.txt"))
-    visited = solve(start, maze, Direction((-1, 0)), side_length, False)
+    start_guard = GuardState(start, Direction((-1, 0)))
+    visited = solve1(start_guard, maze, side_length)
     print(len(visited))
-    import time
-
-    start_time = time.perf_counter()
-    cycle_spots = 0
-    iterations = 0
-    for coord in visited:
-        if coord == start:
-            continue
-        cycle_spots += solve(start, maze + [coord], Direction((-1, 0)), side_length, True)
-        iterations += 1
-        if iterations % 500 == 0:
-            print((time.perf_counter() - start_time) / iterations)
-            print(iterations / len(visited))
-    print(cycle_spots)
-    print(time.perf_counter() - start_time)
+    print(solve2(GuardState(start, Direction((-1, 0))), maze, side_length, visited))
 
 
 if __name__ == "__main__":
