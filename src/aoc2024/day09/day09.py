464d5276ec863e85582e3f64ad98ef2c2abb9e20
Joshua Mitra <joshua@alphalayer.ai>
2024-12-09 15:26:27 -0700

    Move day9 logic out of main
    stop tweaking dammit

--- src/aoc2024/day09/day09.py
+++ src/aoc2024/day09/day09.py
@@ -1,6 +1,7 @@
 import itertools
 from collections import deque
 from pathlib import Path
+from typing import Protocol
 
 from tqdm import tqdm
 
@@ -16,55 +17,77 @@ def read_disk_map(path: Path) -> str:
     return line
 
 
-def fill_gap(group_deque: deque[IndexAndGroup], gap_length: int) -> list[IndexAndGroup]:
-    """
-    Given length of empty space to fill and deque of files in order to fill in return a list of IndexAndGroups to fill
-    in the empty space with and remove these files from the deque.
-    """
-    if gap_length == 0:
-        return []
-    try:
-        next_file = next(itertools.dropwhile(lambda g: len(g[1]) > gap_length, group_deque))
-    except StopIteration:
-        return []
-    # Eww side effect
-    group_deque.remove(next_file)
-    result = [next_file] + fill_gap(group_deque, gap_length - len(next_file[1]))
-    return result
+class PartHandler(Protocol):
+
+    def handle_character(self, index: int, space_length: int, defragged: DiskMap) -> DiskMap: ...
+
+
+class Part1Handler:
+
+    def __init__(self, data_groups: list[IndexAndGroup], line_length: int) -> None:
+        self.data_queue = deque((list(itertools.chain.from_iterable(map(lambda g: g[1], data_groups)))))
+        self.indices_to_fill = set(range(1, line_length, 2))
+
+    def handle_character(self, index: int, space_length: int, defragged: DiskMap) -> DiskMap:
+        id_count = min(space_length, len(self.data_queue))
+        if index in self.indices_to_fill:
+            defragged.extend([self.data_queue.pop() for _ in range(id_count)])
+        else:
+            defragged.extend([self.data_queue.popleft() for _ in range(id_count)])
+        return defragged
+
+
+class Part2Handler:
+    def __init__(self, data_groups: list[IndexAndGroup], line_length: int) -> None:
+        self.group_deque = deque(data_groups)
+        self.indices_to_fill = set(range(1, line_length, 2))
+
+    def handle_character(self, index: int, space_length: int, defragged: DiskMap) -> DiskMap:
+        if index in self.indices_to_fill:
+            self.group_deque.reverse()
+            filler = self.fill_gap(space_length)
+            self.group_deque.reverse()
+            self.indices_to_fill |= set([f[0] for f in filler])
+            fill_data: DiskMap = list(itertools.chain.from_iterable([f[1] for f in filler]))
+            fill_data += (space_length - len(fill_data)) * ["."]
+            defragged.extend(fill_data)
+        else:
+            defragged.extend(list(self.group_deque.popleft()[1]))
+        return defragged
+
+    def fill_gap(self, gap_length: int) -> list[IndexAndGroup]:
+        """
+        Given length of empty space to fill and deque of files in order to fill in return a list of IndexAndGroups to
+        fill in the empty space with and remove these files from the deque.
+        """
+        if gap_length == 0:
+            return []
+        # get the first occurrence that isn't too long without iterating the entire list.
+        # Would just be head $ dropWhile in haskell
+        try:
+            next_file = next(itertools.dropwhile(lambda g: len(g[1]) > gap_length, self.group_deque))
+        except StopIteration:
+            return []
+        self.group_deque.remove(next_file)
+        return [next_file] + self.fill_gap(gap_length - len(next_file[1]))
+
+
+def check_sum(disk_map: DiskMap) -> int:
+    return sum([i * char for i, char in enumerate(disk_map) if isinstance(char, int)])
 
 
 def main() -> None:
     line = read_disk_map(Path("data/input09.txt"))
     data_groups = [(i, tuple(int(line[i]) * [i // 2])) for i in range(0, len(line), 2)]
-    group_deque: deque[IndexAndGroup] = deque(data_groups)
-    data_queue: deque[int] = deque((list(itertools.chain.from_iterable(map(lambda g: g[1], data_groups)))))
-    compressed: DiskMap = []
-    compressed2: DiskMap = []
+    handlers: list[PartHandler] = [Part1Handler(data_groups, len(line)), Part2Handler(data_groups, len(line))]
+    disk_maps: list[DiskMap] = [[], []]
 
-    part1_indices_to_fill: set[int] = set(range(1, len(line), 2))
-    part2_indices_to_fill: set[int] = set(range(1, len(line), 2))
     for i, char in tqdm(enumerate(line)):
-        f_length = int(char)
-        id_count = min(len(data_queue), f_length)
-        if len(group_deque) == 0:
-            break
-        if i in part1_indices_to_fill:
-            compressed.extend([data_queue.pop() for _ in range(id_count)])
-        else:
-            compressed.extend([data_queue.popleft() for _ in range(id_count)])
-        if i in part2_indices_to_fill:
-            group_deque.reverse()
-            filler = fill_gap(group_deque, f_length)
-            group_deque.reverse()
-            part2_indices_to_fill |= set([f[0] for f in filler])
-            fill_data: DiskMap = list(itertools.chain.from_iterable([f[1] for f in filler]))
-            fill_data += (f_length - len(fill_data)) * ["."]
-            compressed2.extend(fill_data)
-        else:
-            compressed2.extend(list(group_deque.popleft()[1]))
+        for j, h in enumerate(handlers):
+            disk_maps[j] = h.handle_character(i, int(char), disk_maps[j])
 
-    print(sum([i * char for i, char in enumerate(compressed) if isinstance(char, int)]))
-    print(sum([i * char for i, char in enumerate(compressed2) if isinstance(char, int)]))
+    print(check_sum(disk_maps[0]))
+    print(check_sum(disk_maps[1]))
 
 
 if __name__ == "__main__":
